5686da89fc9564eee2adbea9b5007d8e9cafe7af
tomdiamond123 <diamondtom099@gmail.com>
2026-06-10 16:34:44 +0100

    Display grid

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 const int fractionAlive {2};
 const int gridHeight {100};
 const int gridWidth {100};
+const float scale {10};
 
 template <typename T, std::size_t Row, std::size_t Col>
 using Array2d = std::array<std::array<T, Col>, Row>;
@@ -24,16 +25,32 @@ Array2d<int, gridHeight, gridWidth> initGrid(){
 	return grid;
 }
 
+void displayGrid(const Array2d<int, gridHeight, gridWidth> grid, sf::RenderWindow& window){
+	Array2d<int, gridHeight, gridWidth> newGrid = grid;
+	for (int row = 0; row<gridHeight; row++){
+		for (int col = 0; col<gridWidth; col++){
+			// std::cout << "row: " << row << "  col: " << col << "  grid[row][col]: " << grid[row][col] << '\n';
+			if (grid[row][col] == 1){
+				sf::RectangleShape square({scale,scale});
+				square.setPosition({static_cast<float>(col*scale),static_cast<float>(row*scale)});
+				// square.setPosition({static_cast<float>(col),static_cast<float>(row)});
+				square.setFillColor(sf::Color(255, 255, 255));
+				window.draw(square);
+			}
+		}
+	}
+}
+
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { gridHeight*10, gridWidth*10 } ), "GameOfLife" );
+	sf::RenderWindow window( sf::VideoMode( { static_cast<unsigned int>(gridHeight*scale), static_cast<unsigned int>(gridWidth*scale) } ), "GameOfLife" );
 	Array2d<int, gridHeight, gridWidth> grid = initGrid();
-	for(auto& row : grid){
-		for(auto& square: row){
-			std::cout << square;
-		}
-		std::cout << '\n';
-	}
+	// for(auto& row : grid){
+	// 	for(auto& square: row){
+	// 		std::cout << square;
+	// 	}
+	// 	std::cout << '\n';
+	// }
 
 
 	while ( window.isOpen() )
@@ -45,7 +62,7 @@ int main()
 		}
 
 		window.clear();
-		//window.draw( shape );
+		displayGrid(grid, window);
 		window.display();
 		
 	}
