11acdbdca5703ec7deb83af2bb9fb2e48feea902
tomdiamond123 <diamondtom099@gmail.com>
2026-06-10 17:29:03 +0100

    added the game of life rules and changed the type alias for the game grid finishing the project

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@ const int gridHeight {100};
 const int gridWidth {100};
 const float scale {10};
 
-template <typename T, std::size_t Row, std::size_t Col>
-using Array2d = std::array<std::array<T, Col>, Row>;
+// template <typename T, std::size_t Row, std::size_t Col>
+// using Array2d = std::array<std::array<T, Col>, Row>;
+using intGameGrid = std::array<std::array<int, gridHeight>, gridWidth>;
 
-Array2d<int, gridHeight, gridWidth> initGrid(){
-	Array2d<int, gridHeight, gridWidth> grid {};
+intGameGrid initGrid(){
+	intGameGrid grid {};
 
 	std::mt19937 mt{std::random_device{}()};//initialise seed
 	std::uniform_int_distribution zeroOrOne{0,1}; // creates random number generator for either 0 or 1
@@ -25,15 +26,12 @@ Array2d<int, gridHeight, gridWidth> initGrid(){
 	return grid;
 }
 
-void displayGrid(const Array2d<int, gridHeight, gridWidth> grid, sf::RenderWindow& window){
-	Array2d<int, gridHeight, gridWidth> newGrid = grid;
-	for (int row = 0; row<gridHeight; row++){
-		for (int col = 0; col<gridWidth; col++){
-			// std::cout << "row: " << row << "  col: " << col << "  grid[row][col]: " << grid[row][col] << '\n';
+void displayGrid(const intGameGrid grid, sf::RenderWindow& window){
+	for (int row {0}; row<gridHeight; row++){
+		for (int col {0}; col<gridWidth; col++){
 			if (grid[row][col] == 1){
 				sf::RectangleShape square({scale,scale});
 				square.setPosition({static_cast<float>(col*scale),static_cast<float>(row*scale)});
-				// square.setPosition({static_cast<float>(col),static_cast<float>(row)});
 				square.setFillColor(sf::Color(255, 255, 255));
 				window.draw(square);
 			}
@@ -41,10 +39,47 @@ void displayGrid(const Array2d<int, gridHeight, gridWidth> grid, sf::RenderWindo
 	}
 }
 
+intGameGrid updateBoardByRules(intGameGrid grid){
+	intGameGrid newGrid {grid};
+
+	for (int row{0}; row<gridHeight; row++){
+		for (int col{0}; col<gridWidth; col++){
+			int numAlive {0};
+
+			for (int diffRow{-1}; diffRow<=1; diffRow++){
+				for (int diffCol{-1}; diffCol<=1; diffCol++){
+					if (diffRow == 0 && diffCol == 0){
+						continue;
+					}
+					//finds neighbour (% allows loop across screen, +gridHeight/gridWidth forces positive numbers)
+					int neighbour {grid[((row+diffRow) + gridHeight) % gridHeight][((col+diffCol) + gridHeight) % gridWidth]};
+					
+					if (neighbour == 1){
+						numAlive += 1;
+					}
+				}
+			}
+
+			if (numAlive < 2 || numAlive > 3){
+				newGrid[row][col] = 0;
+			} 
+			else if(numAlive == 3){
+				newGrid[row][col] = 1;
+			} 
+			else if(numAlive == 2){
+				newGrid[row][col] = grid[row][col];
+			}
+		}
+	}
+
+	return newGrid;
+}
+
 int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { static_cast<unsigned int>(gridHeight*scale), static_cast<unsigned int>(gridWidth*scale) } ), "GameOfLife" );
-	Array2d<int, gridHeight, gridWidth> grid = initGrid();
+	window.setFramerateLimit(10);
+	intGameGrid grid = initGrid();
 	// for(auto& row : grid){
 	// 	for(auto& square: row){
 	// 		std::cout << square;
@@ -61,6 +96,7 @@ int main()
 				window.close();
 		}
 
+		grid = updateBoardByRules(grid);
 		window.clear();
 		displayGrid(grid, window);
 		window.display();
